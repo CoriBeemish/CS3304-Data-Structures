538743c9effe430d66e4cf5d163c495d8db33c6d
CoriBeemish <cbeemish@gmail.com>
2018-02-09 13:43:55 -0500

    Update complexTest.cpp

--- Labs/Lab 2/complexTest.cpp
+++ Labs/Lab 2/complexTest.cpp
@@ -4,27 +4,36 @@
 
 #include <iostream>
 #include <cassert>
+#include <cmath>
 
 int main(int argc, char **argv)
 {
-  complex c1 (-5, -8);
-  complex c2 (2, 8);
-  complex c3 = c1 + c2;
-  assert (c3.get_real() == 7);
-  assert (c3.get_imag() == 8);
+    complex c1 (3, 2);
+    assert (c1.get_real() == 3);
+    assert (c1.get_imag() == 2);
 
-  complex c4 = c1 - c2;
-  assert (c4.get_real() == 3);
-  assert (c4.get_imag() == 8);
+    complex c2 (-4, 3);
 
-  complex c5 = c1 * c2;
-  assert (c5.get_real() == 5);
-  assert (c5.get_imag() == 32);
+    //SUM
+    complex c3 = c1 + c2;
+    assert (c3.get_real() == -1);
+    assert (c3.get_imag() == 5);
 
-  complex c6 = c1 / c2;
-  assert (c6.get_real() == 5);
-  assert (c6.get_imag() == 2);
+    //DIFFERENCE
+    complex c4 = c1 - c2;
+    assert (c4.get_real() == 7);
+    assert (c4.get_imag() == -1);
 
-  std::cout << "passed all tests" << std::endl;
-  return 0;
+    //PRODUCT
+    complex c5 = c1 * c2;
+    assert (c5.get_real() == -18);
+    assert (c5.get_imag() == -6);
+
+    //QUOTIENT
+    complex c6 = c1/ c2;
+    assert (c6.get_real() == -6/25.0);
+    assert (c6.get_imag() == -17/25.0);
+
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
 }
